79ae100fd19a19eb80ab592cbf0f49d0f2163fba
joako <talv4913@gmail.com>
2025-07-28 15:46:49 -0300

    Creacion Serializers, Reserva y disponibilidad

---
+++ res/serializers.py
@@ -0,0 +1,34 @@
+import datetime
+
+from rest_framework import serializers
+
+from .models import ReservaModel
+
+MAXIMO_POR_BLOQUE = 40
+
+class ReservaSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        if data['fecha'] < datetime.date.today():
+            raise serializers.ValidationError("La fecha no puede ser del pasado")
+        return data
+
+    def create(self, validated_data):
+        usuario = self.context['request'].user
+        if not usuario.is_authenticated:
+            raise serializers.ValidationError("Debes estar logueado para hacer una reserva")
+        validated_data['usuario'] = usuario
+        return super().create(validated_data)
+
+    class Meta:
+        model = ReservaModel
+        fields = '__all__'
+        read_only_fields = ['usuario']
+
+class DisponibilidadSerializer(serializers.Serializer):
+    fecha = serializers.DateField()
+    comensales = serializers.IntegerField(min_value=1)
+
+    def validate_fecha(self, valor):
+        if valor < datetime.date.today():
+            raise serializers.ValidationError("La fecha no puede ser del pasado")
+        return valor
